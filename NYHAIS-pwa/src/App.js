384ced3e79a120b9b5b042bb8dc706a27a68ca75
anthony.albertorio <anthony.albertorio@gmail.com>
2019-06-24 16:16:24 -0400

    refactored App.js from boiler plate to start getting website up.
    Sort of ugly, but will fix with time. Something is better than nothing.

--- NYHAIS-pwa/src/App.js
+++ NYHAIS-pwa/src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import { Jumbotron } from 'react-bootstrap';
+import logo from './logo.svg';  // change logo
+import { Header } from './Components/Header.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="New York Health Artificial Intelligence Society"/>
+      <article>
+        <section id="about-us">
+          <h2>About Us</h2>
+          <Jumbotron>
+            <p> New York Health Artificial Intelligence Society (NYHAIS) 
+            is dedicated to the application of neuroscience, 
+            machine learning, medicine, and computer science towards 
+            the further understanding and development of AI. 
+            We also focus on developing the New York MedTech industry and fostering 
+            open source projects that support it. 
+            Contact the organizers about it. </p>
+            
+            <p>Our goal is to create a laid-back place where the 
+            spirit of the coffeehouse culture of the Enlightenment 
+            meets the modern hackerspace; where people from 
+            different disciplines who wouldn't normally meet can exchange 
+            ideas, experiment with hands-on systems, and have a good time. </p>
+
+            <p>We hope this can be the melting pot of 
+            neuroscientists, hackers, programmers, doctors, 
+            mathematicians, artists, lawyers, post docs in 
+            any science, bio-staticians, entrepreneurs, 
+            and the average person who is interested in 
+            learning about advances in artificial intelligence 
+            applications in health 
+            (and adjacent data-intensive fields).</p>
+          </Jumbotron>
+        </section>
+      </article>
+
+      <section id="jobs">
+        <h2 id="jobs">Jobs</h2>
+        <p>some stuff</p>
+      </section>
+
     </div>
   );
 }
 
 export default App;
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
